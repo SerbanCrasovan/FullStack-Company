28238760cbedeeca2be0997d3d66ec3ca7b2079c
Serban Crasovan <serban.crasovan@morningstar.com>
2023-03-21 15:43:56 +0200

    create data models

---
+++ BackEnd/BackEndCompany/BackEndCompany/Data/Models/Comments.cs
@@ -0,0 +1,10 @@
+﻿namespace BackEndCompany.Data.Models
+{
+    public class Comments
+    {
+        public int Id { get; set; }
+        public int Count { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+}
