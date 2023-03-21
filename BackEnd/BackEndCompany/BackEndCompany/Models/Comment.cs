f7bce177f5156925dcd9aa6170ec135b6c393c7a
Serban Crasovan <serban.crasovan@morningstar.com>
2023-03-21 19:36:26 +0200

    Modify data models

--- BackEnd/BackEndCompany/BackEndCompany/Models/Comment.cs
+++ BackEnd/BackEndCompany/BackEndCompany/Models/Comment.cs
@@ -3,7 +3,8 @@
     public class Comment
     {
         public int Id { get; set; }
-        public int Count { get; set; }
+
+        public string Message { get; set; }
 
         public int CompanyId { get; set; }
 
--- BackEnd/BackEndCompany/BackEndCompany/Models/Company.cs
+++ BackEnd/BackEndCompany/BackEndCompany/Models/Company.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Comments> Comments { get; set; } = new HashSet<Comments>();
+        public ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
 
     }
 }
